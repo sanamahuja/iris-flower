444b2cdf45cd73d2895d93da9bbcca31b9119b07
sanamahuja <75626567+sanamahuja@users.noreply.github.com>
2021-11-22 19:36:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ sepal_width = st.slider('Sepal Width',  0.1, 7.9, 2.0)
 petal_length = st.slider('Petal Length',  0.1, 7.9, 2.0)
 petal_width = st.slider('Petal Width',  0.1, 7.9, 2.0)
 
-
-
-
 from sklearn.datasets import load_iris
 iris = load_iris()
 x = iris.data
